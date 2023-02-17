050751eef5951251f1c2b60e1622350045812cf6
Nicolò Padovan <nicolopadovandevelop@gmail.com>
2023-02-17 10:37:58 +0100

    Initial Commit

---
+++ H-Connect/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  H-Connect
+//
+//  Created by Nicolò Padovan on 17/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
